d477866500d9ef6b2c9771fe7e82b2536f5fa8e4
Rerurate514 <rerurateyuto@gmail.com>
2025-04-07 02:58:20 +0900

    indexの更新

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//core
+export { View } from "./core/interface/view";
+export { assembleView } from "./core/logic/assembleView";
+
+//taterials
+export { Text } from "./taterials/components/text";
+

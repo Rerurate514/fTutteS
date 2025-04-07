95b2ce78674b6439ed98f2ded857e4c66e9e8527
Rerurate514 <rerurateyuto@gmail.com>
2025-04-07 21:02:26 +0900

    Columnコンポーネントを追加

---
+++ src/taterials/components/column.ts
@@ -0,0 +1,29 @@
+import { BaseCSS } from "../../cssKit/baseCSS";
+import { View } from "../../core/interface/view";
+
+interface ColumnProps {
+    children: Array<View>;
+    baseCSS: BaseCSS;
+    isHorizontalCenter: boolean
+}
+
+export class Column extends View {
+    constructor(protected props: ColumnProps){
+        super();
+    }
+
+    styledView(element: HTMLElement): HTMLElement{
+        element.style.display = "flex";
+        element.style.flexDirection = "column";
+
+        if(this.props.isHorizontalCenter) element.style.alignItems = "center";
+
+        element = this.props.baseCSS.applyCSS(element);
+
+        return element; 
+    }
+
+    build(): View | Array<View>{
+        return this.props.children;
+    }
+}

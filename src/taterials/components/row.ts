c36a133f9f162441736676d0596532ecbe1aff9d
Rerurate514 <rerurateyuto@gmail.com>
2025-04-08 23:41:42 +0900

    rowコンポーネントを追加

---
+++ src/taterials/components/row.ts
@@ -0,0 +1,35 @@
+import { BaseCSS } from "../../cssKit/baseCSS.js";
+import { View } from "../../core/interface/view.js";
+
+interface RowProps {
+    children: View | Array<View> | undefined;
+    baseCSS?: BaseCSS;
+    isAlignCenter?: boolean;
+    isJustifySpaceAround?: boolean;
+    isJustifySpaceBetween?: boolean;
+    isVerticalCenter?: boolean;
+}
+
+export class Row extends View {
+    constructor(protected props: RowProps) {
+        super();
+    }
+
+    override styledView(element: HTMLElement): HTMLElement {
+        element.className = "row-container";
+        element.style.display = "flex";
+
+        if (this.props.isAlignCenter) element.style.justifyContent = "center";
+        if (this.props.isJustifySpaceAround) element.style.justifyContent = "space-around";
+        if (this.props.isJustifySpaceBetween) element.style.justifyContent = "space-between";
+        if (this.props.isVerticalCenter) element.style.alignItems = "center";
+
+        if (this.props.baseCSS) element = this.props.baseCSS.applyCSS(element);
+
+        return element;
+    }
+
+    override build(): View | Array<View> | undefined {
+        return this.props.children;
+    }
+}

c7431f81839e9d6d8eab9a7831545f4bb2ae3f0e
Rerurate514 <rerurateyuto@gmail.com>
2025-04-09 22:10:13 +0900

    imageコンポーネントを追加

---
+++ src/taterials/components/image.ts
@@ -0,0 +1,31 @@
+import { View } from "../../core/interface/view";
+import { BaseCSS } from "../../cssKit/baseCSS";
+
+interface ImageProps {
+    src: string;
+    alt: string;
+    title?: string;
+    baseCSS?: BaseCSS;
+    //webkitCSS: WebkitCSS;
+}
+
+export class Image extends View {
+    constructor(protected props: ImageProps) {
+        super();
+    }
+
+    createWrapView(): HTMLImageElement {
+        return document.createElement("img");
+    }
+
+    styledView(element: HTMLImageElement): HTMLImageElement {
+        if(this.props.baseCSS) element = this.props.baseCSS.applyCSS(element) as HTMLImageElement;
+        //if(this.props.webkitCSS) element = this.props.webkitCSS.applyCSS(element);
+
+        element.src = this.props.src;
+        element.alt = this.props.alt;
+        if(this.props.title) element.title = this.props.title;
+
+        return element;
+    }
+}

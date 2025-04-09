5fbe3b0e9ab1a12399da8d4d857bb081d034e12d
Rerurate514 <rerurateyuto@gmail.com>
2025-04-09 16:43:47 +0900

    ShadowLevelsの追加

---
+++ src/taterials/enums/shadowLevels.ts
@@ -0,0 +1,13 @@
+enum ShadowLevels {
+    NONE = 'none',
+    LVL1 = '0px 0px 4px rgba(0,0,0,0.1)',
+    LVL2 = '0px 0px 8px rgba(0,0,0,0.12)',
+    LVL3 = '0px 0px 16px rgba(0,0,0,0.14)',
+    LVL4 = '0px 0px 24px rgba(0,0,0,0.16)',
+    LVL5 = '0px 0px 32px rgba(0,0,0,0.18)',
+    LVL6 = '0px 0px 40px rgba(0,0,0,0.20)',
+    LVL7 = '0px 0px 48px rgba(0,0,0,0.22)',
+    LVL8 = '0px 0px 56px rgba(0,0,0,0.24)',
+    LVL9 = '0px 0px 64px rgba(0,0,0,0.26)',
+    LVL10 = '0px 0px 72px rgba(0,0,0,0.28)'
+}

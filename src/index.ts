d7035eefbacef7388838c01c81f9e7f3d0c48e98
Rerurate514 <rerurateyuto@gmail.com>
2025-04-10 23:03:45 +0900

    exportパスを設定

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,4 @@
-//core
-export { View } from "./core/interface/view";
-export { assembleView } from "./core/logic/assembleView";
-
-//taterials
-export { Card } from './taterials/components/card';
-export { Center } from './taterials/components/center';
-export { Column } from './taterials/components/column';
-export { ElevatedButton } from './taterials/components/elevatedButton';
-export { Grid } from './taterials/components/grid';
-export { Header } from './taterials/components/header';
-export { Hover } from './taterials/components/hover';
-export { Image } from './taterials/components/image';
-export { Link } from './taterials/components/link';
-export { Margin } from './taterials/components/margin';
-export { Padding } from './taterials/components/padding';
-export { Position } from './taterials/components/position';
-export { RadioButton } from './taterials/components/radioButton';
-export { RelativePosition } from './taterials/components/relativePosition';
-export { Row } from './taterials/components/row';
-export { Scaffold } from './taterials/components/scaffold';
-export { Shrink } from './taterials/components/shrink';
-export { Slider } from './taterials/components/slider';
-export { SpaceBox } from './taterials/components/spaceBox';
-export { Stack } from './taterials/components/stack';
-export { Text } from './taterials/components/text';
-export { TextArea } from './taterials/components/textArea';
-export { TextForm } from './taterials/components/textForm';
-export { Transform } from './taterials/components/transform'
-
-export { RelativePositions } from './taterials/enums/relativePositions';
-export { ShadowLevels } from './taterials/enums/shadowLevels';
-
-//tiperes
-export { LimitedProviderScope } from './tiperes/components/limitedProviderScope';
-export { ProviderScope } from './tiperes/interface/providerScope';
-export { Provider } from './tiperes/logic/provider';
-export { ProviderObserver } from './tiperes/logic/observer';
-
-//cssKit
-export { BaseCSS } from './cssKit/baseCSS';
-export { BorderCSS } from './cssKit/borderCSS';
-export { FontCSS } from './cssKit/fontCSS';
-export { TextCSS } from './cssKit/textCSS';
+export * from './core';
+export * from './taterials';
+export * from './tiperes';
+export * from './cssKit';

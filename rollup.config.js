4f4b15c52d9ab392eac7ed3862019aa01a508c8a
Rerurate514 <rerurateyuto@gmail.com>
2025-04-11 01:08:04 +0900

    alias設定

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,100 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-    input: [
-        'src/index.ts',
-        'src/core/index.ts',
-        'src/taterials/index.ts',
-        'src/cssKit/index.ts',
-        'src/tiperes/index.ts'
-    ],
-    output: {
-        dir: './dist',
-        format: 'esm',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        paths: {
-            '@core': './dist/core',
-            '@taterials': './dist/taterials',
-            '@cssKit': './dist/cssKit',
-            '@tiperes': './dist/tiperes'
-        }
+export default [
+    {
+        input: [
+            'src/index.ts',
+            'src/core/index.ts',
+            'src/taterials/index.ts',
+            'src/cssKit/index.ts',
+            'src/tiperes/index.ts'
+        ],
+        output: {
+            dir: './dist',
+            format: 'cjs', // CommonJS 形式
+            preserveModules: true,
+            preserveModulesRoot: 'src',
+            paths: {
+                '@core': './dist/core',
+                '@taterials': './dist/taterials',
+                '@cssKit': './dist/cssKit',
+                '@tiperes': './dist/tiperes'
+            },
+            sourcemap: true, // 必要に応じて
+            entryFileNames: '[name].js' // CommonJS の出力ファイル名
+        },
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                moduleResolution: 'node',
+                rootDir: './src'
+            }),
+            nodeResolve(),
+            terser({
+                compress: {
+                    dead_code: true,
+                    conditionals: true,
+                    collapse_vars: true
+                },
+                mangle: {
+                    keep_classnames: true,
+                    keep_fnames: true
+                },
+                format: {
+                    comments: false,
+                    beautify: false
+                },
+                ecma: 2015
+            })
+        ]
     },
-    plugins: [
-        typescript({
-            tsconfig: './tsconfig.json',
-            moduleResolution: 'node',
-            rootDir: './src'
-        }),
-        terser({
-            compress: {
-                dead_code: true,
-                conditionals: true,
-                collapse_vars: true
+    {
+        input: [
+            'src/index.ts',
+            'src/core/index.ts',
+            'src/taterials/index.ts',
+            'src/cssKit/index.ts',
+            'src/tiperes/index.ts'
+        ],
+        output: {
+            dir: './dist',
+            format: 'es', // ES Modules 形式
+            preserveModules: true,
+            preserveModulesRoot: 'src',
+            paths: {
+                '@core': './dist/core',
+                '@taterials': './dist/taterials',
+                '@cssKit': './dist/cssKit',
+                '@tiperes': './dist/tiperes'
             },
-            mangle: {
-                keep_classnames: true,
-                keep_fnames: true
-            },
-            format: {
-                comments: false,
-                beautify: false
-            },
-            ecma: 2015
-        })
-    ]
-};
+            sourcemap: true, // 必要に応じて
+            entryFileNames: '[name].mjs' // ES Modules の出力ファイル名
+        },
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                moduleResolution: 'node',
+                rootDir: './src'
+            }),
+            nodeResolve(),
+            terser({
+                compress: {
+                    dead_code: true,
+                    conditionals: true,
+                    collapse_vars: true
+                },
+                mangle: {
+                    keep_classnames: true,
+                    keep_fnames: true
+                },
+                format: {
+                    comments: false,
+                    beautify: false
+                },
+                ecma: 2015
+            })
+        ]
+    }
+];

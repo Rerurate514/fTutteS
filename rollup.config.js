5bf8bdccdba544d62ecbaabb38a21b39dc8687c9
Rerurate514 <rerurateyuto@gmail.com>
2025-04-11 01:21:46 +0900

    git revert 2629e3b

--- rollup.config.js
+++ rollup.config.js
@@ -1,100 +1,33 @@
 import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default [
-    {
-        input: [
-            'src/index.ts',
-            'src/core/index.ts',
-            'src/taterials/index.ts',
-            'src/cssKit/index.ts',
-            'src/tiperes/index.ts'
-        ],
-        output: {
-            dir: './dist',
-            format: 'cjs', // CommonJS 形式
-            preserveModules: true,
-            preserveModulesRoot: 'src',
-            paths: {
-                '@core': './dist/core',
-                '@taterials': './dist/taterials',
-                '@cssKit': './dist/cssKit',
-                '@tiperes': './dist/tiperes'
-            },
-            sourcemap: true, // 必要に応じて
-            entryFileNames: '[name].js' // CommonJS の出力ファイル名
+export default {
+    input: 'dist/index.js',
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'cjs'
         },
-        plugins: [
-            typescript({
-                tsconfig: './tsconfig.json',
-                moduleResolution: 'node',
-                rootDir: './src'
-            }),
-            nodeResolve(),
-            terser({
-                compress: {
-                    dead_code: true,
-                    conditionals: true,
-                    collapse_vars: true
-                },
-                mangle: {
-                    keep_classnames: true,
-                    keep_fnames: true
-                },
-                format: {
-                    comments: false,
-                    beautify: false
-                },
-                ecma: 2015
-            })
-        ]
-    },
-    {
-        input: [
-            'src/index.ts',
-            'src/core/index.ts',
-            'src/taterials/index.ts',
-            'src/cssKit/index.ts',
-            'src/tiperes/index.ts'
-        ],
-        output: {
-            dir: './dist',
-            format: 'es', // ES Modules 形式
-            preserveModules: true,
-            preserveModulesRoot: 'src',
-            paths: {
-                '@core': './dist/core',
-                '@taterials': './dist/taterials',
-                '@cssKit': './dist/cssKit',
-                '@tiperes': './dist/tiperes'
+        {
+            file: 'dist/index.mjs',
+            format: 'es'
+        }
+    ],
+    plugins: [
+        terser({
+            compress: {
+                dead_code: true,
+                conditionals: true,
+                collapse_vars: true
             },
-            sourcemap: true, // 必要に応じて
-            entryFileNames: '[name].mjs' // ES Modules の出力ファイル名
-        },
-        plugins: [
-            typescript({
-                tsconfig: './tsconfig.json',
-                moduleResolution: 'node',
-                rootDir: './src'
-            }),
-            nodeResolve(),
-            terser({
-                compress: {
-                    dead_code: true,
-                    conditionals: true,
-                    collapse_vars: true
-                },
-                mangle: {
-                    keep_classnames: true,
-                    keep_fnames: true
-                },
-                format: {
-                    comments: false,
-                    beautify: false
-                },
-                ecma: 2015
-            })
-        ]
-    }
-];
+            mangle: {
+                keep_classnames: true,
+                keep_fnames: true
+            },
+            format: {
+                comments: false,
+                beautify: false
+            },
+            ecma: 2015
+        })
+    ]
+};

0dc0b467a9e01a8250977be63b61002e2b2327db
Rerurate514 <rerurateyuto@gmail.com>
2025-05-17 02:51:22 +0900

    tommand実行エントリポイントを作成

---
+++ src/tommand/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { initCommand } from './modules/commands/init';
+
+export function run(){
+    const program = new Command();
+
+    program
+        .name("tommand")
+        .description("setup ftuttes project")
+        .version("0.1.0");
+
+    program.addCommand(initCommand);
+
+    program.parse(process.argv);
+
+    if (require.main === module) {
+        program.parse(process.argv);
+    }
+}

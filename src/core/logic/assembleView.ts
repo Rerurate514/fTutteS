62b9bd63b17f302c63e8e989fbbad48859f847c9
Rerurate514 <rerurateyuto@gmail.com>
2025-05-23 06:48:12 +0900

    assembleViewDocの生成

--- src/core/logic/assembleView.ts
+++ src/core/logic/assembleView.ts
@@ -1,5 +1,29 @@
 import { View } from "../interface/view";
 
+/**
+ * assembleView関数
+ * ## OverView
+ * fTutteSアプリケーションのエントリーポイントとなる関数です。
+ * 指定されたルート`View`ウィジェットを組み立て、それを指定されたIDのHTMLコンテナ要素に追加します。
+ * `View`のライフサイクル（`assemble()`、`assembleComplete()`）を適切に呼び出し、
+ * UIの初期描画と後処理を管理します。
+ *
+ * ## Parameters
+ * @param viewArg - 必須 アプリケーションのルートとなるViewインスタンス。
+ * @param idName - オプション Viewが追加されるHTMLコンテナ要素のID。デフォルトは`"fTutteS-Container"`。
+ * - このIDを持つ要素がDOM内に存在しない場合、Viewは追加されません。
+ *
+ * ## Returns
+ * @returns {HTMLElement} - DOMに追加されたルートViewのHTMLElement。
+ * 
+ * ## Examples
+ * @example
+ * ```typescript
+ * assembleView(
+ *   new Text({text: "Hello World"});
+ * );
+ * ```
+ */
 export function assembleView(
     viewArg: View,
     idName: string = "fTutteS-Container"

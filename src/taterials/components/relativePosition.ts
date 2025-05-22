f1c5668519426eba070ca1fb22383c08f764896b
Rerurate514 <rerurateyuto@gmail.com>
2025-05-23 06:22:17 +0900

    relativePositionDocの生成

--- src/taterials/components/relativePosition.ts
+++ src/taterials/components/relativePosition.ts
@@ -7,6 +7,60 @@ interface RelativePositionProps {
     };
 }
 
+/**
+ * RelativePositionコンポーネント
+ * ## OverView
+ * 子要素を親要素に対して相対的に配置します。
+ * `relativePositions`プロパティを通じて、具体的な相対位置のスタイルを適用します。
+ *
+ * ## Props
+ * @param props - RelativePositionの設定オプション
+ * @param props.child - 必須 相対的に配置する子要素
+ * @param props.relativePositions - 必須 位置に関するCSSスタイルを適用するオブジェクト
+ * - このオブジェクトは`RelativePositions`を渡す必要があります。
+ *
+ * ## Examples
+ * 基本的な使用例
+ * @example
+ * ```typescript
+ * new RelativePosition({
+ *   relativePositions: RelativePositions.BOTTOM_LEFT,
+ *   child: new Card({
+ *     baseCSS: new BaseCSS({
+ *       background: "lightgray",
+ *       width: "100px",
+ *       height: "50px"
+ *     }),
+ *     child: new Text({ text: "lower left" })
+ *   })
+ * });
+ * ```
+ *
+ * 他のRelativePositionsの例
+ * @example
+ * ```typescript
+ * new RelativePosition({
+ *   relativePositions: RelativePositions.CENTER,
+ *   child: new Card({
+ *     baseCSS: new BaseCSS({
+ *       background: "lightgray",
+ *       width: "100px",
+ *       height: "50px"
+ *     }),
+ *     child: new Text({ text: "center" })
+ *   })
+ * });
+ * ```
+ *
+ * 最小限の設定 (注: `relativePositions`は必須です)
+ * @example
+ * ```typescript
+ * new RelativePosition({
+ *   relativePositions: RelativePositions.TOP_LEFT, // デフォルトのRelativePositionsを使用
+ *   child: new Text({ text: "upper left" })
+ * });
+ * ```
+ */
 export class RelativePosition extends View {
     constructor(protected props: RelativePositionProps) {
         super();
